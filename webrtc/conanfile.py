995b8c7a12cafa2003064dc32100c471511ce315
Markus Lanner <contact@markus-lanner.com>
2020-06-04 19:01:41 +0200

    raised version v84. disabled all CI

--- webrtc/conanfile.py
+++ webrtc/conanfile.py
@@ -6,8 +6,8 @@ class WebrtcConan(ConanFile):
     # versions https://chromiumdash.appspot.com/releases?platform=Linux
     # the version 83.0.4103.61 means v83, branch head 4103 (daily branches)
     # https://groups.google.com/forum/#!msg/discuss-webrtc/Ozvbd0p7Q1Y/M4WN2cRKCwAJ
-    version = "83"
-    _branchHead = "4103"
+    version = "84"
+    _branchHead = "4147"
     license = "MIT"
     author = "Markus Lanner <contact@markus-lanner.com>"
     url = "github.com/freckled-dev/conan-google-webrtc"
